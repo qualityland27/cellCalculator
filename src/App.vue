1d87353d2b6a7d5ec2cd83980fa2aa0780296ac1
Qualityland27 <qualityland@gmx.de>
2022-05-10 13:26:57 +0200

    impressum fix

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,16 @@
     <img  src="./assets/background.jpg" >
   </div>
   <div id="nav" class="absolute top-0 w-screen content-center items-center flex justify-center text-white bg-volkert-blue h-20 opacity-80">
-    <a class="mx-2" href="https://www.volkert.net/">Volkert Webseite</a> |
-    <router-link class="mx-2" to="/">Home </router-link> |
-    <router-link class="mx-2" to="/about">About</router-link>
+    <a class="mx-2" href="https://www.volkert.net/">Home</a> 
+    <!-- <router-link class="mx-2" to="/">Home </router-link>  | -->
+    <!-- <router-link class="mx-2" to="/about">About</router-link> -->
   </div>
 </div>  
 
   <router-view/>
   <footer class="content-center items-center flex justify-center text-white bg-volkert-blue h-20 opacity-80">
         <!-- <router-link class="mx-2" to="/about">Impressum</router-link> -->
-        <a class="mx-2" href="https://www.https://www.volkert.net/Kontakt/Impressum/">Impressum</a>
+        <a class="mx-2" href="https://www.volkert.net/Kontakt/Impressum/">Impressum</a>
   </footer>
 </template>
 
